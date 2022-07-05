33af4e1724e0af3b39ba55046bbf21d1fbf2557c
nshaw928 <nshaw928@gmail.com>
2022-07-05 10:00:52 -0400

    Added python to prep for pi score submission

---
+++ pi_score_prep.py
@@ -0,0 +1,12 @@
+# Import Packages
+import pandas as pd
+import os
+
+# Open dataset folder
+
+
+# Check each set of proteins to make sure the run completed
+
+
+# Create New Dataset with only the pdb, named with proteins and total alignment error (A_B_TAE)
+
