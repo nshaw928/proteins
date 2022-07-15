4923838b7a56422e8737873203c6241c6e9f7b6e
nshaw928 <nshaw928@gmail.com>
2022-07-15 15:25:12 -0400

    updated with correct paths

--- piscore.sh
+++ piscore.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -c 6
-#SBATCH --gres=gpu:1
-#SBATCH --partition=gpu_requeue
-#SBATCH --error=af2_multi_tz.err
-#SBATCH --output=af2_multi_tz.out
-#SBATCH -t 0-23:00:00
-#SBATCH --mem=40G
-#SBATCH --requeue
+#SBATCH -c 2
+#SBATCH --partition=short
+#SBATCH --error=piscore.err
+#SBATCH --output=piscore.out
+#SBATCH -t 0-1:00:00
+#SBATCH --mem=5G
 
-alphafold.py --fasta_paths=/n/scratch3/users/n/nis806/tz_full_data/$1 --max_template_date=2020-05-14 --db_preset=full_dbs --output_dir=/n/scratch3/users/n/nis806/tz_full_results/ --data_dir=/n/shared_db/alphafold/ --model_preset=multimer --run_relax --num_multimer_predictions_per_model=1
+ccpem-python /nfs/sbgrid/programs/x86_64-linux/ccpem/1.6.0/lib/py2/ccpem/src/ccpem_core/tasks/atomic_model_validation/bfactor_analysis/analyse_bfactors.pyc /n/scratch3/users/n/nis806/piscore_result/
