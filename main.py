02ae8fb20d35a168e773d183db002418ff17f0c8
nshaw928 <nshaw928@gmail.com>
2022-07-14 23:41:30 -0400

    outlined file

--- main.py
+++ main.py
@@ -5,3 +5,12 @@
 
 
 # Check datasets for stage of project
+
+
+# Function creates all combinations of FASTA files from one
+
+
+# Function creates pdb files for PI scoring and adds rank and PLDDT scores to df
+
+
+# Function adds results of PI scoring to df
