b644414aec8b092722bc53d34faf3bb47271d22f
nshaw928 <nshaw928@gmail.com>
2022-07-17 20:21:54 -0400

    create data folders if they dont exist

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 # Import Packages
+import pandas as pd
+import os
+import shutil
+import json
 
+# Check for datasets folder, and if it does not exist make it
+first_time = True
+home_path = os.getcwd()
+for item in home_path:
+    if home_path + '\\' + item == home_path + '\\' + 'data':
+        first_time = False
+    else:
+        pass
+if first_time:
+   os.makedirs('data')
+   os.chdir('data')
+   dirs = ['fasta_initial', 'fasta_result', 'alphafold_result', 'piscore_data', 'piscore_result']
+   for dir in dirs:
+       os.makedirs(dir)
 
 # Set path variables
 
