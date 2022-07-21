167662ec6705ce4d0688c1060a388091647c4c28
nshaw928 <nshaw928@gmail.com>
2022-07-21 17:41:05 -0400

    PI scores get added to df

--- main.py
+++ main.py
@@ -294,20 +294,33 @@ def piscore_extract():
                         df_features.rename(columns={'pdb': 'index'}, inplace=True)
                         # Adds features to df the first time
                         if df.shape[1] == columns_df or df.shape[1] == (columns_df + 3):
-                            df = pd.merge(df, df_features, on='index', how='outer')
-                        # Adds features to df every other time
-                        else:
-                            df = pd.concat([df, df_features], keys='index')
+                            df = df.merge(df_features, on='index', how='outer')
+                        # Adds features to df every other time, doing this through assignment
+                        if df_features.shape[0] == 1:
+                            df.loc[pair_index, 'Num_intf_residues'] = df_features.loc[0, 'Num_intf_residues']
+                            df.loc[pair_index, 'Polar'] = df_features.loc[0, 'Polar']
+                            df.loc[pair_index, 'Hydrophobhic'] = df_features.loc[0, 'Hydrophobhic']
+                            df.loc[pair_index, 'Charged'] = df_features.loc[0, 'Charged']
+                            df.loc[pair_index, 'contact_pairs'] = df_features.loc[0, 'contact_pairs']
+                            df.loc[pair_index, ' sc'] = df_features.loc[0, ' sc']
+                            df.loc[pair_index, ' hb'] = df_features.loc[0, ' hb']
+                            df.loc[pair_index, ' sb'] = df_features.loc[0, ' sb']
+                            df.loc[pair_index, ' int_solv_en'] = df_features.loc[0, ' int_solv_en']
+                            df.loc[pair_index, ' int_area'] = df_features.loc[0, ' int_area']
+                            df.loc[pair_index, ' pvalue'] = df_features.loc[0, ' pvalue']
+                    else:
+                        pass
                     # Get PIS csv and add to df
                     if data.split('_')[0] == 'pi':
                         df_pis = pd.read_csv(pis_path + '\\' + data)
                         df_pis.rename(columns={'#PDB': 'index'}, inplace=True)
                         # Adds PIS to the df the first time
                         if df.shape[1] == columns_df or df.shape[1] == (columns_df + 12):
-                            df = pd.merge(df, df_pis, on='index', how='outer')
+                            df = df.merge(df_pis, on='index', how='outer')
                         # Adds PIS to df every other time
-                        else:
-                            df = pd.concat([df, df_pis], keys='index')
+                        if df_pis.shape[0] == 1:
+                            df.loc[pair_index, 'predicted_class'] = df_pis.loc[0, 'predicted_class']
+                            df.loc[pair_index, 'pi_score'] = df_pis.loc[0, 'pi_score']
                     else:
                         pass
 
