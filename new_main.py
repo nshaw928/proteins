3c394598c7c5858e6a6ebd386b8e3daeefd1facd
nshaw928 <nshaw928@gmail.com>
2022-08-01 21:31:01 -0400

    formatting

--- new_main.py
+++ new_main.py
@@ -8,7 +8,8 @@ protein_complex = 'zync_finger_with_IFTB'
 
 # Run start function to create folders and set save paths
 home_path, data_path, fasta_initial_path, fasta_result_path, alphafold_result_path, piscore_data_path,\
-    piscore_result_path = start(af_path='C:\\Users\\nshaw\\OneDrive\\Desktop\\Projects\\proteins\\complexes\\zync_finger_with_IFTB\\alphafold_result', protein_complex=protein_complex)
+    piscore_result_path = start(af_path='C:\\Users\\nshaw\\OneDrive\\Desktop\\Projects\\proteins\\complexes'
+                                        '\\zync_finger_with_IFTB\\alphafold_result', protein_complex=protein_complex)
 
 # Run generate_fasta function to generate fasta files to be used as AlphaFold input
 generate_fasta(run_type='1_with_all', home_path=home_path, data_path=data_path, fasta_initial_path=fasta_initial_path,
