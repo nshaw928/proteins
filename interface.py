1187ccca31356e8145298dd8e4fb1b8cc746688f
nshaw928 <nshaw928@gmail.com>
2022-08-02 00:19:24 -0400

    added file for finding interface residues

---
+++ interface.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import Bio.PDB as bio
+
+def interface():
+# Load PDB file
+
+# Split PDB file into chains
+
+# Create list of atoms in each chain
+
+# Get residues on both chains within __ A of each other
+
+
+# df = df.astype('object') makes dataframe able to hold lists
