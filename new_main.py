2e09374a44f1b97e985392f88fc9774a40b159cb
nshaw928 <nshaw928@gmail.com>
2022-08-10 18:07:59 -0400

    added dropbox file generation

--- new_main.py
+++ new_main.py
@@ -8,8 +8,7 @@ protein_complex = '172_IFTA'
 
 # Run start function to create folders and set save paths
 home_path, data_path, fasta_initial_path, fasta_result_path, alphafold_result_path, piscore_data_path,\
-    piscore_result_path, pdb_for_dropbox = start(af_path='C:\\Users\\nshaw\\OneDrive\\Desktop\\Projects\\proteins\\complexes'
-                                        '\\zync_finger_with_IFTB\\alphafold_result', protein_complex=protein_complex)
+    piscore_result_path, pdb_for_dropbox = start(af_path='', protein_complex=protein_complex)
 
 # Run generate_fasta function to generate fasta files to be used as AlphaFold input
 generate_fasta(run_type='1_with_all', home_path=home_path, data_path=data_path, fasta_initial_path=fasta_initial_path,
@@ -17,7 +16,7 @@ generate_fasta(run_type='1_with_all', home_path=home_path, data_path=data_path, 
 
 # Saves the top ranked alphafold prediction for pi scoring as index for associated interaction from pre_piscore.csv
 generate_pdbs_from_af(fasta_result_path=fasta_result_path, alphafold_result_path=alphafold_result_path,
-                      piscore_data_path=piscore_data_path, data_path=data_path)
+                      piscore_data_path=piscore_data_path, data_path=data_path, pdb_for_dropbox=pdb_for_dropbox)
 
 # Extracts pi score from the results of pi score program
 #extract_piscore(data_path=data_path, piscore_result_path=piscore_result_path)
