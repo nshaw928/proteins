e436847c6fdf92ae550076e1d577383d5b0412ab
nshaw928 <nshaw928@gmail.com>
2022-08-10 19:15:48 -0400

    added alphafold path var

--- new_main.py
+++ new_main.py
@@ -4,11 +4,12 @@ from generate_pdbs_from_af import generate_pdbs_from_af
 from extract_piscore import extract_piscore
 
 # Set which protein complex you are working on
-protein_complex = '172_IFTA'
+protein_complex = '139_IFTB'
+af = 'F:\\alphafold_data\\p139_IFTB_result'
 
 # Run start function to create folders and set save paths
 home_path, data_path, fasta_initial_path, fasta_result_path, alphafold_result_path, piscore_data_path,\
-    piscore_result_path, pdb_for_dropbox = start(af_path='', protein_complex=protein_complex)
+    piscore_result_path, pdb_for_dropbox = start(af_path=af, protein_complex=protein_complex)
 
 # Run generate_fasta function to generate fasta files to be used as AlphaFold input
 generate_fasta(run_type='1_with_all', home_path=home_path, data_path=data_path, fasta_initial_path=fasta_initial_path,
