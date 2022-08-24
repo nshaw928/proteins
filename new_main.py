93f38285fcfaf51345e09a645aedc0a563ef8c85
nshaw928 <nshaw928@gmail.com>
2022-08-24 12:07:59 -0400

    added dropbox path

--- new_main.py
+++ new_main.py
@@ -2,22 +2,27 @@ from start import start
 from generate_fasta import generate_fasta
 from generate_pdbs_from_af import generate_pdbs_from_af
 from extract_piscore import extract_piscore
+import os
 
 # Set which protein complex you are working on
-protein_complex = '139_IFTB'
-af = 'F:\\alphafold_data\\p139_IFTB_result'
+protein_complex = 'CytC'
+af = 'F:\\alphafold_data\\CytC_result'
+
+home_path = os.getcwd()
 
 # Run start function to create folders and set save paths
-home_path, data_path, fasta_initial_path, fasta_result_path, alphafold_result_path, piscore_data_path,\
+data_path, fasta_initial_path, fasta_result_path, alphafold_result_path, piscore_data_path,\
     piscore_result_path, pdb_for_dropbox = start(af_path=af, protein_complex=protein_complex)
 
 # Run generate_fasta function to generate fasta files to be used as AlphaFold input
-generate_fasta(run_type='1_with_all', home_path=home_path, data_path=data_path, fasta_initial_path=fasta_initial_path,
-               fasta_result_path=fasta_result_path)
+# run_type all = every pair of proteins
+# run_type 1_with_all = the first protein with every other
+#generate_fasta(run_type='all', home_path=home_path, data_path=data_path, fasta_initial_path=fasta_initial_path,
+#               fasta_result_path=fasta_result_path)
 
 # Saves the top ranked alphafold prediction for pi scoring as index for associated interaction from pre_piscore.csv
-generate_pdbs_from_af(fasta_result_path=fasta_result_path, alphafold_result_path=alphafold_result_path,
-                      piscore_data_path=piscore_data_path, data_path=data_path, pdb_for_dropbox=pdb_for_dropbox)
+#generate_pdbs_from_af(fasta_result_path=fasta_result_path, alphafold_result_path=alphafold_result_path,
+#                      piscore_data_path=piscore_data_path, data_path=data_path, pdb_for_dropbox=pdb_for_dropbox)
 
 # Extracts pi score from the results of pi score program
-#extract_piscore(data_path=data_path, piscore_result_path=piscore_result_path)
+extract_piscore(data_path=data_path, piscore_result_path=piscore_result_path)
