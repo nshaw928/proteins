81e912bbc055b69a8fae801797a623345c2b5934
nshaw928 <nshaw928@gmail.com>
2022-08-26 16:41:03 -0400

    added selinium pisa crawler

--- utils.py
+++ utils.py
@@ -42,5 +42,71 @@ def parse_xml(path_to_xml):
     print(df)
     return df
 
-xml_path = 'datasets\\xml_files\\ranked_0.xml'
-parse_xml(xml_path)
+#xml_path = 'datasets\\xml_files\\ranked_0.xml'
+#parse_xml(xml_path)
+
+
+# Start of web crawler
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+def run_pisa(pdb_file, xml_files_path):
+
+    # Checks if an element exists on the current page
+    def check_exists_by_name(name, cur_driver):
+        try:
+            cur_driver.find_element(by=By.NAME, value=name)
+        except NoSuchElementException:
+            return False
+        return True
+
+    # Load Firefox without GUI
+    opts = Options()
+    #opts.add_argument("--headless")
+    driver = Firefox(options=opts)
+
+    # Go to the website
+    driver.get("https://www.ebi.ac.uk/msd-srv/prot_int/pistart.html")
+    driver.implicitly_wait(1)
+
+    driver.find_element(by=By.NAME, value='start_server').click()
+    driver.implicitly_wait(2)
+
+    # Find the radio button for uploading a coordinate file
+    driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[2]/div/form/table/tbody/tr[4]/td/u/input').click()
+    driver.implicitly_wait(3)
+
+    # Upload the PDB file that we want to run PISA on
+    driver.find_element(by=By.NAME, value='file_upload').send_keys(pdb_file)
+    driver.implicitly_wait(2)
+    driver.find_element(by=By.NAME, value='btn_upload').click()
+
+    # Wait for upload to finish and submit button to appear
+    while not check_exists_by_name('btn_submit_interfaces', driver):
+        pass
+
+    # Submit pdb to PISA
+    driver.find_element(by=By.NAME, value='btn_submit_interfaces').click()
+
+    # Figure out if contacts are found
+    if driver.find_element(by=By.CLASS_NAME, value='phead').text.startswith('No'):
+        print("No interface found")
+
+    # Contacts are found, extract data from XML
+    else:
+        while not check_exists_by_name('downloadXML', driver):
+            pass
+        # Get XML data
+        driver.find_element(by=By.NAME, value='downloadXML').click()
+        driver.implicitly_wait(1)
+        # Go to window with XML data
+        driver.switch_to.window(driver.window_handles[1])
+        xml_url = driver.current_url
+
+        # Saves name of input file for identification
+        file_name = pdb_file.split('\\')[-1].split('.')[0]
+        print(driver.page_source)
+        with open(xml_files_path + '/' + file_name + '.xml', 'w') as file:
+            file.write(driver.page_source)
