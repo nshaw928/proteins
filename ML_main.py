0db67caa58ab42164cf1257750a4e9041bb9b1bd
nshaw <nshaw928@gmail.com>
2022-11-01 02:21:04 -0400

    commented stuff out, and set up for new parsing function from utils

--- ML_main.py
+++ ML_main.py
@@ -1,21 +1,30 @@
-from utils import parse_xml, run_pisa_online, compile_features
+from utils import run_pisa, parse_xml, compile_features
 
 # General Imports
 import pandas as pd
 import os
 
+test_xml = 'data/117e_outfile'
+
+parse_xml(test_xml)
+
+
+
+
+
 
 
 # Run PISA
 pdb_folder = 'D:\\Data\\Aug26_PDB'
-xml_files_path = 'datasets/xml_files/Sep7'
+xml_files_path = 'data/xml_files/Sep7'
+
 #for file in os.listdir(pdb_folder):
 #    file_name = pdb_folder + '\\' + file
-#    run_pisa_online(file_name, xml_files_path)
+#    run_pisa(file_name, xml_files_path)
 
 # Compile all xml files into single df
-path_to_xml_folder = 'datasets/xml_files'
-compile_features(path_to_xml_folder)
+#path_to_xml_folder = 'datasets/xml_files'
+#compile_features(path_to_xml_folder)
 print('FINSIHED MAIN')
 
 
