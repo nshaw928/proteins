07ab52f322fe81cad37ac5cc9647037049fa8e8a
nshaw <nshaw928@gmail.com>
2022-11-03 15:42:51 -0400

    updated parse function

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import time
+import xml.etree.ElementTree as ET
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
@@ -16,9 +17,90 @@ def run_pisa(path_to_pdb_folder):
         output_file = file.split('.')[0] + '_pisa.xml'
         os.system('./pisa name -xml interfaces >' + output_file)
 
+# NEW PARSE FUNCTION
+def parse_xml(path_to_xml):
+    # Save the name of the proteins
+    proteinA = path_to_xml.split('_')[0]
+    proteinB = path_to_xml.split('_')[1]
+    proteins = proteinA + '_' + proteinB
+
+    # Load the xml file into the parse system
+    tree = ET.parse(path_to_xml)
+    root = tree.getroot()
+
+    # Define a dataframe to save the interface to
+    df = pd.DataFrame({
+        'area': [],
+        'deltag': [],
+        'pvalue': [],
+        'hbonds': [],
+        'saltbridges': [],
+        'avg_res': [],
+    })
+
+    # Separate each interface and extract features
+    for interface in root.findall('interface'):
+        area = interface.find('int_area').text
+        deltag = interface.find('int_solv_en').text
+        pvalue = interface.find('pvalue').text
+
+        # Save the number of each type of interaction as a var
+        for hbond in interface.findall('h-bonds'):
+            hbonds = int(hbond.find('n_bonds').text)
+        for saltbridge in interface.findall('salt-bridges'):
+            saltbridges = int(saltbridge.find('n_bonds').text)
+
+        # Save the number of residues from each molecule
+        counter = 0
+        for molecule in interface.findall('molecule'):
+            if counter == 0:
+                chain1_res = int(molecule.find('int_nres').text)
+                counter += 1
+            else:
+                chain2_res = int(molecule.find('int_nres').text)
+
+        # Save all for this particular interface into a temporary df
+        temp_df = pd.DataFrame({
+        'area': [area],
+        'deltag': [deltag],
+        'pvalue': [pvalue],
+        'hbonds': [hbonds],
+        'saltbridges': [saltbridges],
+        'tot_res': [(chain1_res + chain2_res)],
+        })
+
+        # Append the temporary df to the main one
+        df = pd.concat([df, temp_df])
+
+    # Filters out small interfaces
+    df = df[df['tot_res'] > 2]
+
+    # Add weighted delta G to new column
+    df['weighted_deltag'] = int(df.deltag) * int(df.tot_res)
+
+    # Summarize the interface data into a single row
+    # Save summaries of features as vars
+    sum_hbonds = df['hbonds'].sum()
+    sum_saltbridges = df['saltbridges'].sum()
+    sum_area = df['area'].sum()
+    sum_tot_res = df['tot_res'].sum()
+    deltag_interaction = ((df['weighted_deltag'].sum()) / (sum_tot_res))
+
+    # Summarize df in single row of new_df
+    new_df = pd.DataFrame({
+        'protA_protB': [proteins],
+        'hbonds': [sum_hbonds],
+        'saltbridges': [sum_saltbridges],
+        'area': [sum_area],
+        'deltag': [deltag_interaction], # This is the weighted delta g
+        'tot_res': [sum_tot_res],
+    })
+
+    # Returns new_df which is a single row summarizing the interaction
+    return new_df
 
 # Parse XML file and extract relevant interface information
-def parse_xml(path_to_xml):
+def parse_xml_fromweb(path_to_xml):
     '''
     Specifically designed to parse a PISA generated xml file
     :return: a df containing select interface features
